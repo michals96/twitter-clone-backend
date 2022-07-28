a1b31e4c21c903a5bcaa18e8b832b6b21b43fa0e
michals96 <michal.stefaniuk9@gmail.com>
2022-07-28 20:43:30 +0200

    Working shape

--- web/src/main/java/twitter/web/WebApplication.java
+++ web/src/main/java/twitter/web/WebApplication.java
@@ -2,8 +2,14 @@ package twitter.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan({"twitter.commons", "twitter.web", "twitter.core.tweet"})
+@EnableJpaRepositories({"twitter.core.tweet"})
+@EntityScan({"twitter.core.tweet"})
 public class WebApplication {
 
     public static void main(String[] args) {
